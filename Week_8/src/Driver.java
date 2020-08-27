718b8fc8f51b6011a4fa8da5e4cb9ab57ef6ced4
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-27 14:26:33 -0600

    commit 08-27-2020 1426 hrs.

---
+++ Week_8/src/Driver.java
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------------------
+//Lab8_Ex1 Kip Brower  08/25/20 CPSC 50100-002
+//Driver.java
+//-----------------------------------------------------------------------
+public class Driver {
+final static int QUEUE_LIMIT = 7;
+	static int front, rear, count;
+static int [] queue;
+	public static void main(String[] args) {
+	enqueue(1);	
+	enqueue(7);	
+	enqueue(3);	
+	enqueue(4);	
+	enqueue(9);	
+	enqueue(2);	
+	
+	System.out.println(count);
+	}
+public static void ArrayQ() {
+	front = rear = count = 0;
+	queue = new int [QUEUE_LIMIT];
+}
+
+public static void enqueue(int a) {
+	queue[rear] = a;
+	rear = (rear+1) %queue.length;
+	count++;
+}
+public static int arrayCount() {
+	return count;
+}
+
+}
---
+++ Week_8/src/Lab8_Ex3.java
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------
+//Lab8_Ex3 Kip Brower  08/25/20 CPSC 50100-002
+//Answers to Exercise 3 questions
+//-----------------------------------------------------------------------
+public class Lab8_Ex3 {
+
+	
+	a) What is the root node of the tree? - Node 50
+	b) What are the leaf nodes of the tree? - Nodes 9, 14, 19, 67, and 76
+	c) What are the ancestor nodes of the node containing node 19? - 23, 17, 50
+	d) Write the sequence of node values that you would get from a post-order traversal. 
+		- 9, 14, 12, 19, 23, 17, 67, 54, 76, 72, 50.
+}
