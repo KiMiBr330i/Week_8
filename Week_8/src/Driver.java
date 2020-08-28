3ad519abb33f823340367f20038a475962b8ff83
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-27 18:47:24 -0600

    commit 08-27-20, 1847 hrs.

--- Week_8/src/Driver.java
+++ Week_8/src/Driver.java
@@ -3,32 +3,85 @@
 //Driver.java
 //-----------------------------------------------------------------------
 public class Driver {
-final static int QUEUE_LIMIT = 7;
-	static int front, rear, count;
-static int[] queue;
-	public static void main(String[] args) {
-	enqueue(1);	
-	enqueue(7);	
-	enqueue(3);	
-	enqueue(4);	
-	enqueue(9);	
-	enqueue(2);	
+	final static int QUEUE_LIMIT = 7;
+	static int frontAQ, rearAQ, countAQ;
+	static int[] queue = new int[QUEUE_LIMIT];
+	static LinkedNode frontLQ, rearLQ;
+	static int countLQ;
+
+	public static void main(String[] args) throws NullPointerException {
+		enqueueLQ(1);
+		enqueueLQ(7);
+		dequeueLQ();
+		
+		
+
+	}
+
+	public static void ArrayQ() {
+		frontAQ = rearAQ = countAQ = 0;
+
+	}
+
+	public static void enqueueAQ(int a) {
+		queue[rearAQ] = a;
+		rearAQ = (rearAQ + 1) % queue.length;
+		countAQ++;
+
+	}
+
+	public static int arrayCount() {
+		return countAQ;
+	}
+
+	public static int dequeueAQ() {
+		if (countAQ == 0)
+			return -1;
+		int b = queue[frontAQ];
+		frontAQ = (frontAQ + 1) % queue.length;
+		countAQ--;
+		System.out.println(b);
+		return b;
+
+	}
+
+	public void LinkedQueue() {
+		frontLQ = rearLQ = null;
+		countLQ = 0;
+	}
+
+	public static void enqueueLQ(int x) {
+		LinkedNode newNode = new LinkedNode();
+		if (rearLQ != null)
+			rearLQ.next = newNode;
+		else
+			frontLQ = newNode;
+		rearLQ = newNode;
+		countLQ++;	
+		
+	}
+
+	public static int dequeueLQ() {
+		if (frontLQ == null) {
+			return -1;
+		}
+		if (frontLQ != rearLQ) {
+			int x = frontLQ.c;
+			frontLQ = frontLQ.next;
+			countLQ--;
+			return x;
+			
+		} else {
+			int x = frontLQ.c;
+			frontLQ = null;
+			rearLQ = null;
+			countLQ--;
+			return x;
+			
+		}
+
+	}
 	
-	System.out.println(count);
+		
 	}
-public static void ArrayQ() {
-	front = rear = count = 0;
-	queue = new int[QUEUE_LIMIT];
-	}
-
-
-public static void enqueue(int a) {
-	queue[rear] = a;
-	rear = (rear+1) %queue.length;
-	count++;
-}
-public static int arrayCount() {
-	return count;
-}
-
-}
+	
---
+++ Week_8/src/LinkedNode.java
@@ -0,0 +1,12 @@
+
+public class LinkedNode {
+int c;
+LinkedNode next;
+
+public void LinkedNode() {
+	next = null;
+}
+public void LinkedNode(int c) {
+this.c = c;
+}
+}
